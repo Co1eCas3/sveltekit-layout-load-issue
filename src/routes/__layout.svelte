9e8172f613b0257469bcc4bebb9e016fe2779fa3
Cole Case <colecaseprogram@gmail.com>
2022-07-16 17:04:50 -0500

    added some explanatory console.logs

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,6 @@
 <script context="module">
   export async function load() {
+    console.log("load");
     return {
       props: { someVar: "something" },
     };
@@ -8,12 +9,18 @@
 
 <script>
   export let someVar;
+  $: console.log("someVar: ", someVar);
+
+  function changeSomeVar() {
+    console.log("editing someVar");
+    someVar += " else;";
+  }
 </script>
 
 <main>
   <header>
     <h1>{someVar}</h1>
-    <button on:click={() => (someVar += " else")}>Change somevar</button>
+    <button on:click={changeSomeVar}>Change somevar</button>
   </header>
 
   <section>
